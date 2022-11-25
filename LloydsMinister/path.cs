c38be50f2ce1e2bd467b3ec3e0dcba65507ea48d
Daanyaal-Ilyas <93329164+Daanyaal-Ilyas@users.noreply.github.com>
2022-11-25 07:56:45 +0000

    path

--- LloydsMinister/path.cs
+++ LloydsMinister/path.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace LloydsMinister
 {
-    internal class path
+    public class path
     {
+        public static string path1 = (@"Data Source=C:\Users\omaid\OneDrive\Documents\GitHub\LloydsMinister\LloydsMinister\customer.db3");
+        public static string path2 = (@"C:\Users\omaid\OneDrive\Documents\GitHub\LloydsMinister\LloydsMinister\customer.db3");
     }
 }
