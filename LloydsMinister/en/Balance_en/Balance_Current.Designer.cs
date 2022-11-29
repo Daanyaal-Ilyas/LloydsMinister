2cc93bd0e59cddfc0c83c1b58e6e65f3ddf40f4c
Daanyaal-Ilyas <93329164+Daanyaal-Ilyas@users.noreply.github.com>
2022-11-29 23:46:08 +0000

    fix

--- LloydsMinister/en/Balance_en/Balance_Current.Designer.cs
+++ LloydsMinister/en/Balance_en/Balance_Current.Designer.cs
@@ -53,18 +53,18 @@
             // BalanceCurrentextra3btn
             // 
             this.BalanceCurrentextra3btn.BackgroundImage = global::LloydsMinister.Properties.Resources.Triangle1;
-            this.BalanceCurrentextra3btn.Location = new System.Drawing.Point(754, 239);
+            this.BalanceCurrentextra3btn.Location = new System.Drawing.Point(737, 239);
             this.BalanceCurrentextra3btn.Name = "BalanceCurrentextra3btn";
-            this.BalanceCurrentextra3btn.Size = new System.Drawing.Size(48, 52);
+            this.BalanceCurrentextra3btn.Size = new System.Drawing.Size(65, 84);
             this.BalanceCurrentextra3btn.TabIndex = 9;
             this.BalanceCurrentextra3btn.TabStop = false;
             // 
             // btnBalanceBack
             // 
             this.btnBalanceBack.BackgroundImage = global::LloydsMinister.Properties.Resources.Triangle1;
-            this.btnBalanceBack.Location = new System.Drawing.Point(754, 367);
+            this.btnBalanceBack.Location = new System.Drawing.Point(737, 367);
             this.btnBalanceBack.Name = "btnBalanceBack";
-            this.btnBalanceBack.Size = new System.Drawing.Size(48, 50);
+            this.btnBalanceBack.Size = new System.Drawing.Size(65, 84);
             this.btnBalanceBack.TabIndex = 10;
             this.btnBalanceBack.TabStop = false;
             this.btnBalanceBack.Click += new System.EventHandler(this.btnBalanceBack_Click);
@@ -83,7 +83,7 @@
             this.BalanceCurrentextra2btn.BackgroundImage = global::LloydsMinister.Properties.Resources.Triangle_point_to_right;
             this.BalanceCurrentextra2btn.Location = new System.Drawing.Point(0, 239);
             this.BalanceCurrentextra2btn.Name = "BalanceCurrentextra2btn";
-            this.BalanceCurrentextra2btn.Size = new System.Drawing.Size(47, 50);
+            this.BalanceCurrentextra2btn.Size = new System.Drawing.Size(65, 84);
             this.BalanceCurrentextra2btn.TabIndex = 7;
             this.BalanceCurrentextra2btn.TabStop = false;
             // 
@@ -92,7 +92,7 @@
             this.BalanceCurrentextra4btn.BackgroundImage = global::LloydsMinister.Properties.Resources.Triangle_point_to_right;
             this.BalanceCurrentextra4btn.Location = new System.Drawing.Point(0, 367);
             this.BalanceCurrentextra4btn.Name = "BalanceCurrentextra4btn";
-            this.BalanceCurrentextra4btn.Size = new System.Drawing.Size(47, 53);
+            this.BalanceCurrentextra4btn.Size = new System.Drawing.Size(65, 84);
             this.BalanceCurrentextra4btn.TabIndex = 6;
             this.BalanceCurrentextra4btn.TabStop = false;
             // 
@@ -109,7 +109,7 @@
             // 
             this.lbBalCurrentBack.AutoSize = true;
             this.lbBalCurrentBack.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.lbBalCurrentBack.Location = new System.Drawing.Point(679, 376);
+            this.lbBalCurrentBack.Location = new System.Drawing.Point(664, 388);
             this.lbBalCurrentBack.Name = "lbBalCurrentBack";
             this.lbBalCurrentBack.Size = new System.Drawing.Size(67, 32);
             this.lbBalCurrentBack.TabIndex = 13;
